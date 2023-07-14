a8656f935800b6b28bc0aead440a4f77a7218c89
Andres_Pinto <andrespinto4000@gmail.com>
2023-07-13 20:20:19 -0400

    Updated YPLibraryVC

--- Source/SelectionsGallery/YPSelectionsGalleryVC.swift
+++ Source/SelectionsGallery/YPSelectionsGalleryVC.swift
@@ -6,159 +6,16 @@
 //  Copyright © 2018 Yummypets. All rights reserved.
 //
 
-//import UIKit
-//
-//public class YPSelectionsGalleryVC: UIViewController, YPSelectionsGalleryCellDelegate {
-//
-//    override public var prefersStatusBarHidden: Bool { return YPConfig.hidesStatusBar }
-//
-//    public var items: [YPMediaItem] = []
-//    public var didFinishHandler: ((_ gallery: YPSelectionsGalleryVC, _ items: [YPMediaItem]) -> Void)?
-//    private var lastContentOffsetX: CGFloat = 0
-//
-//    var v = YPSelectionsGalleryView()
-//    public override func loadView() { view = v }
-//
-//    public required init(items: [YPMediaItem],
-//                         didFinishHandler:
-//        @escaping ((_ gallery: YPSelectionsGalleryVC, _ items: [YPMediaItem]) -> Void)) {
-//        super.init(nibName: nil, bundle: nil)
-//        self.items = items
-//        self.didFinishHandler = didFinishHandler
-//    }
-//
-//    public required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    override public func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Register collection view cell
-//        v.collectionView.register(YPSelectionsGalleryCell.self, forCellWithReuseIdentifier: "item")
-//        v.collectionView.dataSource = self
-//        v.collectionView.delegate = self
-//
-//        // Setup navigation bar
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: YPConfig.wordings.next,
-//                                                            style: .done,
-//                                                            target: self,
-//                                                            action: #selector(done))
-//        navigationItem.rightBarButtonItem?.tintColor = YPConfig.colors.tintColor
-//        navigationItem.rightBarButtonItem?.setFont(font: YPConfig.fonts.rightBarButtonFont, forState: .disabled)
-//        navigationItem.rightBarButtonItem?.setFont(font: YPConfig.fonts.rightBarButtonFont, forState: .normal)
-//        navigationController?.navigationBar.setTitleFont(font: YPConfig.fonts.navigationBarTitleFont)
-//
-//        YPHelper.changeBackButtonIcon(self)
-//        YPHelper.changeBackButtonTitle(self)
-//    }
-//
-//    @objc
-//    private func done() {
-//        // Save new images to the photo album.
-//        if YPConfig.shouldSaveNewPicturesToAlbum {
-//            for m in items {
-//                if case let .photo(p) = m, let modifiedImage = p.modifiedImage {
-//                    YPPhotoSaver.trySaveImage(modifiedImage, inAlbumNamed: YPConfig.albumName)
-//                }
-//            }
-//        }
-//        didFinishHandler?(self, items)
-//    }
-//
-//    public func selectionsGalleryCellDidTapRemove(cell: YPSelectionsGalleryCell) {
-//        if let indexPath = v.collectionView.indexPath(for: cell) {
-//            items.remove(at: indexPath.row)
-//            v.collectionView.performBatchUpdates({
-//                v.collectionView.deleteItems(at: [indexPath])
-//            }, completion: { _ in })
-//        }
-//    }
-//}
-//
-//// MARK: - Collection View
-//extension YPSelectionsGalleryVC: UICollectionViewDataSource {
-//    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return items.count
-//    }
-//
-//    public func collectionView(_ collectionView: UICollectionView,
-//                               cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item",
-//                                                            for: indexPath) as? YPSelectionsGalleryCell else {
-//            return UICollectionViewCell()
-//        }
-//        cell.delegate = self
-//        let item = items[indexPath.row]
-//        switch item {
-//        case .photo(let photo):
-//            cell.imageView.image = photo.image
-//            cell.setEditable(YPConfig.showsPhotoFilters)
-//        case .video(let video):
-//            cell.imageView.image = video.thumbnail
-//            cell.setEditable(YPConfig.showsVideoTrimmer)
-//        }
-//        cell.removeButton.isHidden = YPConfig.gallery.hidesRemoveButton
-//        return cell
-//    }
-//}
-//
-//extension YPSelectionsGalleryVC: UICollectionViewDelegate {
-//
-//    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let item = items[indexPath.row]
-//        var mediaFilterVC: IsMediaFilterVC?
-//        switch item {
-//        case .photo(let photo):
-//            if !YPConfig.filters.isEmpty, YPConfig.showsPhotoFilters {
-//                mediaFilterVC = YPPhotoFiltersVC(inputPhoto: photo, isFromSelectionVC: true)
-//            }
-//        case .video(let video):
-//            if YPConfig.showsVideoTrimmer {
-//                mediaFilterVC = YPVideoFiltersVC.initWith(video: video, isFromSelectionVC: true)
-//            }
-//        }
-//
-//        mediaFilterVC?.didSave = { outputMedia in
-//            self.items[indexPath.row] = outputMedia
-//            collectionView.reloadData()
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//        mediaFilterVC?.didCancel = {
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//        if let mediaFilterVC = mediaFilterVC as? UIViewController {
-//            let navVC = UINavigationController(rootViewController: mediaFilterVC)
-//            navVC.navigationBar.isTranslucent = false
-//            navVC.navigationBar.backgroundColor = YPConfig.colors.defaultNavigationBarColor
-//            present(navVC, animated: true, completion: nil)
-//        }
-//    }
-//
-//    // Set "paging" behaviour when scrolling backwards.
-//    // This works by having `targetContentOffset(forProposedContentOffset: withScrollingVelocity` overriden
-//    // in the collection view Flow subclass & using UIScrollViewDecelerationRateFast
-//    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        let isScrollingBackwards = scrollView.contentOffset.x < lastContentOffsetX
-//        scrollView.decelerationRate = isScrollingBackwards
-//            ? UIScrollView.DecelerationRate.fast
-//            : UIScrollView.DecelerationRate.normal
-//        lastContentOffsetX = scrollView.contentOffset.x
-//    }
-//}
-
-//Code2
-
 import UIKit
 
 public class YPSelectionsGalleryVC: UIViewController, YPSelectionsGalleryCellDelegate {
-    
+
     override public var prefersStatusBarHidden: Bool { return YPConfig.hidesStatusBar }
-    
+
     public var items: [YPMediaItem] = []
     public var didFinishHandler: ((_ gallery: YPSelectionsGalleryVC, _ items: [YPMediaItem]) -> Void)?
     private var lastContentOffsetX: CGFloat = 0
-    
+
     var v = YPSelectionsGalleryView()
     public override func loadView() { view = v }
 
@@ -169,11 +26,11 @@ public class YPSelectionsGalleryVC: UIViewController, YPSelectionsGalleryCellDel
         self.items = items
         self.didFinishHandler = didFinishHandler
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override public func viewDidLoad() {
         super.viewDidLoad()
 
@@ -181,26 +38,17 @@ public class YPSelectionsGalleryVC: UIViewController, YPSelectionsGalleryCellDel
         v.collectionView.register(YPSelectionsGalleryCell.self, forCellWithReuseIdentifier: "item")
         v.collectionView.dataSource = self
         v.collectionView.delegate = self
-        
+
         // Setup navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: YPConfig.wordings.next,
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(done))
+        navigationItem.rightBarButtonItem?.tintColor = YPConfig.colors.tintColor
+        navigationItem.rightBarButtonItem?.setFont(font: YPConfig.fonts.rightBarButtonFont, forState: .disabled)
+        navigationItem.rightBarButtonItem?.setFont(font: YPConfig.fonts.rightBarButtonFont, forState: .normal)
         navigationController?.navigationBar.setTitleFont(font: YPConfig.fonts.navigationBarTitleFont)
 
-        // Add "Next" and "Manage" buttons to the navigation bar.
-        let nextBarButton = UIBarButtonItem(title: YPConfig.wordings.next,
-                                                style: .done,
-                                                target: self,
-                                                action: #selector(done))
-        nextBarButton.tintColor = YPConfig.colors.tintColor
-        nextBarButton.setFont(font: YPConfig.fonts.rightBarButtonFont, forState: .disabled)
-        nextBarButton.setFont(font: YPConfig.fonts.rightBarButtonFont, forState: .normal)
-
-        let manageBarButton = UIBarButtonItem(title: "Manage", style: .plain, target: self, action: #selector(manageButtonTapped))
-
-        navigationItem.rightBarButtonItems = [nextBarButton, manageBarButton]
-
-
-        navigationItem.rightBarButtonItems = [nextBarButton, manageBarButton]
-        
         YPHelper.changeBackButtonIcon(self)
         YPHelper.changeBackButtonTitle(self)
     }
@@ -217,30 +65,7 @@ public class YPSelectionsGalleryVC: UIViewController, YPSelectionsGalleryCellDel
         }
         didFinishHandler?(self, items)
     }
-    
-    @objc private func manageButtonTapped() {
-        let actionSheet = UIAlertController(title: "Manage Photos", message: "Choose an option", preferredStyle: .actionSheet)
 
-        let selectMorePhotosAction = UIAlertAction(title: "Select More Photos", style: .default) { _ in
-            // Put your logic for selecting more photos here
-        }
-
-        let changeSettingsAction = UIAlertAction(title: "Change Settings", style: .default) { _ in
-            // Navigate user to settings
-            if let url = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-        }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
-        actionSheet.addAction(selectMorePhotosAction)
-        actionSheet.addAction(changeSettingsAction)
-        actionSheet.addAction(cancelAction)
-
-        self.present(actionSheet, animated: true, completion: nil)
-    }
-    
     public func selectionsGalleryCellDidTapRemove(cell: YPSelectionsGalleryCell) {
         if let indexPath = v.collectionView.indexPath(for: cell) {
             items.remove(at: indexPath.row)
@@ -256,7 +81,7 @@ extension YPSelectionsGalleryVC: UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return items.count
     }
-    
+
     public func collectionView(_ collectionView: UICollectionView,
                                cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item",
@@ -279,7 +104,7 @@ extension YPSelectionsGalleryVC: UICollectionViewDataSource {
 }
 
 extension YPSelectionsGalleryVC: UICollectionViewDelegate {
-    
+
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let item = items[indexPath.row]
         var mediaFilterVC: IsMediaFilterVC?
@@ -293,7 +118,7 @@ extension YPSelectionsGalleryVC: UICollectionViewDelegate {
                 mediaFilterVC = YPVideoFiltersVC.initWith(video: video, isFromSelectionVC: true)
             }
         }
-        
+
         mediaFilterVC?.didSave = { outputMedia in
             self.items[indexPath.row] = outputMedia
             collectionView.reloadData()
@@ -309,7 +134,7 @@ extension YPSelectionsGalleryVC: UICollectionViewDelegate {
             present(navVC, animated: true, completion: nil)
         }
     }
-    
+
     // Set "paging" behaviour when scrolling backwards.
     // This works by having `targetContentOffset(forProposedContentOffset: withScrollingVelocity` overriden
     // in the collection view Flow subclass & using UIScrollViewDecelerationRateFast
@@ -321,3 +146,6 @@ extension YPSelectionsGalleryVC: UICollectionViewDelegate {
         lastContentOffsetX = scrollView.contentOffset.x
     }
 }
+
+//Code2
+
