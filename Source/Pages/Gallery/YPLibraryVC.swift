588b59aa7fe131db8c0684ae4a1fbafffa71975d
Andres_Pinto <andrespinto4000@gmail.com>
2023-07-13 20:14:40 -0400

    Updated YPLibraryVC

--- Source/Pages/Gallery/YPLibraryVC.swift
+++ Source/Pages/Gallery/YPLibraryVC.swift
@@ -724,8 +724,20 @@ internal final class YPLibraryVC: UIViewController, YPPermissionCheckable {
 
     // Additional function to present the photo picker.
     func showPhotoPicker() {
-        // Your photo picker presentation code here
+        var config = YPImagePickerConfiguration()
+        let picker = YPImagePicker(configuration: config)
+        
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
     }
+
     
     
     
